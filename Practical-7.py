ecf3e453cda3e3c431ff884a18a008b679e2875c
shubham5155 <149155764+shubham5155@users.noreply.github.com>
2023-11-08 10:57:15 +0530

    Update Practical-7.py

--- Practical-7.py
+++ Practical-7.py
@@ -1,1 +1,16 @@
+#Function that accepts two strings and returns the indices of all occurences of second stirng in the first string as a list
 
+def fdiff(str1, str2):
+    
+    if str1 == str2:
+        return -1
+    else:
+        for str1, str2 in zip(str1, str2):
+            if str1 != str2:
+                return str1
+
+
+string1 = input("Enter first string:")
+string2 = input("Enter second string:")
+print(fdiff(string1, string2))
+    
