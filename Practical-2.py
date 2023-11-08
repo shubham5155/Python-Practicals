f230c0c673834b71a4e7b4f80ea165375e7d5835
shubham5155 <149155764+shubham5155@users.noreply.github.com>
2023-11-08 10:54:54 +0530

    Update Practical-2.py

--- Practical-2.py
+++ Practical-2.py
@@ -1,1 +1,49 @@
+#Program to check whether a number is prime or not
+
+
+a=int(input('Enter the number that you want to check:'))
+if (a>=1):
+    for i in range(2,a):
+        if(a%i==0):
+            print(a,'is not a prime number')
+            break
+    else:
+        print(a,'is a prime number')
+else:
+    print(a,"is not a prime number")
+    
+    
+    
+#Gernerating  prime number till between two intervals; 
+
+
+a=int(input('Enter the first interval : '))
+b=int(input('Enter the second interval : '))
+
+print('The prime number in the given range are:')
+
+for number in range(a, b + 1):
+   if (number>1):
+         for i in range(2,number):
+          if (number%i==0):
+            break
+         else:
+           print(number)
+           
+           
+#Genrating prime numbers till n(any given value)
+
+a=1
+b=int(input('Enter the number upto which you want to print prime numberes:'))
+
+
+for number in range(a, b+1):
+   if (number>1):
+         for i in range(2,number):
+          if (number%i==0):
+            break
+         else:
+           print(number)
+
+
 
