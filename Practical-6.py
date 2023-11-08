d4d5df08aaa9eb3d353305c8b88a4c44b07d4277
shubham5155 <149155764+shubham5155@users.noreply.github.com>
2023-11-08 10:56:50 +0530

    Update Practical-6.py

--- Practical-6.py
+++ Practical-6.py
@@ -1,1 +1,12 @@
+#Program to swap the first character of two strings
+str1 = input("Please Enter First String : ")
+str2 =input("Please Enter Second String : ")
 
+a=str1[0:2]
+
+str1=str1.replace(str1[0:2],str2[0:2])
+
+str2=str2.replace(str2[0:2],a)
+
+print("Your first string has become :- ",str1)
+print("Your second string has become :- ",str2)
